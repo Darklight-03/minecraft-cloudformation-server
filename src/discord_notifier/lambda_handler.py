c8664b3d8b96b112c96f7027f459633572fe615f
Bo Corman <bdc3142@gmail.com>
2021-09-30 21:21:54 -0700

    added ability to add more mods/configs to server (#33)
    * stop sending dumb message every time server starts
    
    * added ability to add custom mods and configs
    
    * added discord integration config and modified deploy script to copy the bot token into it

--- src/discord_notifier/lambda_handler.py
+++ src/discord_notifier/lambda_handler.py
@@ -18,26 +18,33 @@ def lambda_handler(event, context):
     print(f"event {event}")
     url = WEBHOOK_URL
     stack_name = STACK_NAME
+    send_message = False
 
     message_handler = MessageHandler()
 
     if "discord_message" in event.keys():
+        send_message = True
         message_handler.set_message(event["discord_message"])
 
     if "desired_state" in event.keys():
+        send_message = True
         desired_state = event["desired_state"]
         Parameter().set_canstart("True" if desired_state == "Running" else "False")
         if desired_state == "Stopped":
             cfn = ServerManager(stack_name)
             cfn.stop_server()
 
-    result = requests.post(url, json=message_handler.message)
+    if send_message:
+        result = requests.post(url, json=message_handler.message)
 
-    try:
-        result.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(err)
-        return 1
+        try:
+            result.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(err)
+            return 1
+        else:
+            print(f"Payload delivered successfully, code {result.status_code}.")
+            return 0
+
     else:
-        print(f"Payload delivered successfully, code {result.status_code}.")
         return 0
